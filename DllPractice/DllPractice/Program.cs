afc56ae13e852a97e04850203ed9ac026bf06721
MuniaAbdalla <monyaabdullah7596@gmail.com>
2019-11-14 16:17:08 +0300

    First Leasson Done in DotNet Course

---
+++ DllPractice/DllPractice/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Honor;
+namespace DllPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            student s1 = new student("monya", 25898, 3.68);
+            Console.WriteLine(CheckHonor.Check(s1.gpa));
+            Console.ReadLine();
+        }
+    }
+
+    class student {
+        public string name;
+        public int id;
+        public double gpa;
+
+        public student() {
+            name = "";
+            id = 0;
+            gpa = 0.0;
+
+        }
+        public student( string n, int i, double g)
+        {
+            name = n;
+            id = i;
+            gpa = g;
+        }
+
+
+
+    }
+}
---
+++ DllPractice/Honor/CheckHonor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Honor
+{
+    public class CheckHonor
+    {
+       
+        public static bool Check (double GPA) {
+            if (GPA > 3.5)
+                return true;
+            else
+                return false;
+
+        }
+
+    }
+}
